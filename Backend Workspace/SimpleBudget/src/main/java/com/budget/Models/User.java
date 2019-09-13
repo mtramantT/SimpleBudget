ceaa09271a21607d22b84bca59b0c0585cd12dc3
Chirag Matta <chiragamatta@gmail.com>
2019-09-12 21:53:12 -0500

    user pojo

---
+++ Backend Workspace/SimpleBudget/src/main/java/com/budget/Models/User.java
@@ -0,0 +1,77 @@
+package com.budget.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id//used to indicate field is the primary key
+	@GeneratedValue//used to indicate that it will be auto generated
+	private Integer userId;
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private String password;
+	private String email;
+	
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName + ", userName="
+				+ userName + ", password=" + password + ", email=" + email + "]";
+	}
+	public User(Integer userId, String firstName, String lastName, String userName, String password, String email) {
+		super();
+		this.userId = userId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.password = password;
+		this.email = email;
+	}
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(String userName, String password) {
+		super();
+		this.userName = userName;
+		this.password = password;
+	}
+}
---
+++ Backend Workspace/SimpleBudget/src/main/java/com/budget/repo/UserRepository.java
@@ -0,0 +1,9 @@
+package com.budget.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.budget.Models.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+	
+}
